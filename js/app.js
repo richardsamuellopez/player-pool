713b2f07019fda873c5e8ef24ae498843697d379
richardsamuellopez <richardsamuellopez@gmail.com>
2016-03-18 12:19:42 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,11 +14,11 @@ angular
       parseNumbers:true,
     });
   })
-.controller('PoolCtrl',['$interval'], function($scope, Tabletop, $filter, $timeout, $interval){
+.controller('PoolCtrl', function($scope, Tabletop, $filter, $timeout, $interval){
   $interval(function(){
     console.log("A");
     //Tabletop.fetch();
-  },3000);
+  //},3000);
     Tabletop.then(function(ttdata){
       console.log("B2");
       var data = ttdata[0];
@@ -91,5 +91,5 @@ angular
             return player.StillAlive==="No";
           };
     });
-  //},3000);
+  },3000);
 });
