5da682d079aa410f6e3800d51e1ea0b76753599a
richardsamuellopez <richardsameullopez@gmail.com>
2018-01-28 23:05:47 -0500

    Try to disable submit after 1 click

--- js/appEntry2018.js
+++ js/appEntry2018.js
@@ -12,55 +12,60 @@ angular
       wanted:["Seed1","Seed2","Seed3","Seed4","Seed5","Seed6","Seed7","Seed8","Seed9","Seed10","Seed11","Seed12","Seed13","Seed14","Seed15","Seed16"]
     });
 
-  })
+})
 .controller('PoolCtrl', function($scope, Tabletop, $filter, $timeout, $interval){
-$scope.currData="";
-$scope.prevData="";
-$scope.currPlayers="";
-$scope.prevPlayers="";
+  $scope.currData="";
+  $scope.prevData="";
+  $scope.currPlayers="";
+  $scope.prevPlayers="";
   LoadData();
   // $interval(LoadData,97000);
-$scope.clearSearch = function(){
-$scope.searchText="";
-};
+  $scope.clearSearch = function(){
+    $scope.searchText="";
+  };
 
-$scope.reloadPage = function(){window.location.reload();}
-    function LoadData(){Tabletop.then(function(ttdata){
-      var currTS = new Date();
-      // console.log("Load Data: ",currTS);
-      var data = ttdata[0];
-      $scope.prevData=$scope.currData;
-      $scope.currData=data;
+  $scope.reloadPage = function(){window.location.reload();}
+  $scope.isSubmitDisabled = false;
+  $scope.oneClickCheck = function() {
+    if(seed1Player && seed2Player && seed3Player && seed4Player && seed5Player && seed6Player && seed7Player && seed8Player && seed9Player && seed10Player && WC1Player && WC2Player )
+    $scope.isSubmitDisabled = true;
+  };
+  function LoadData(){Tabletop.then(function(ttdata){
+    var currTS = new Date();
+    // console.log("Load Data: ",currTS);
+    var data = ttdata[0];
+    $scope.prevData=$scope.currData;
+    $scope.currData=data;
 
-      $scope.email="";
-      $scope.entryName="";
-      $scope.Seed1=loadSeedData(data.Seed1.elements);
-      $scope.Seed2=loadSeedData(data.Seed2.elements);
-      $scope.Seed3=loadSeedData(data.Seed3.elements);
-      $scope.Seed4=loadSeedData(data.Seed4.elements);
-      $scope.Seed5=loadSeedData(data.Seed5.elements);
-      $scope.Seed6=loadSeedData(data.Seed6.elements);
-      $scope.Seed7=loadSeedData(data.Seed7.elements);
-      $scope.Seed8=loadSeedData(data.Seed8.elements);
-      $scope.Seed9=loadSeedData(data.Seed9.elements);
-      $scope.Seed10=loadSeedData(data.Seed10.elements);
-      $scope.WC1=loadSeedData(_.union(data.Seed11.elements, data.Seed12.elements,data.Seed13.elements,data.Seed14.elements,data.Seed15.elements,data.Seed16.elements));
-      $scope.WC2=$scope.WC1;
-      function loadSeedData(SeedData){
-        var teams = [];// _.filter(SeedData, function(o) { return o.Team != "NO" && isNaN(o.Team); });
-        var players=[];
-        var currTeam = ""
-        for(x=0; x<SeedData.length; x++){
-          if(SeedData[x].Team!="NO" && isNaN(SeedData[x].Team) && SeedData[x].Team!="--"){
-            currTeam = SeedData[x].Team
-            teams.push({name: currTeam, value: currTeam});
-          } else {
-            if(SeedData[x].Team!="NO")
-              players.push({team: currTeam, name: SeedData[x].Player, value: SeedData[x].Player})
-          }
+    $scope.email="";
+    $scope.entryName="";
+    $scope.Seed1=loadSeedData(data.Seed1.elements);
+    $scope.Seed2=loadSeedData(data.Seed2.elements);
+    $scope.Seed3=loadSeedData(data.Seed3.elements);
+    $scope.Seed4=loadSeedData(data.Seed4.elements);
+    $scope.Seed5=loadSeedData(data.Seed5.elements);
+    $scope.Seed6=loadSeedData(data.Seed6.elements);
+    $scope.Seed7=loadSeedData(data.Seed7.elements);
+    $scope.Seed8=loadSeedData(data.Seed8.elements);
+    $scope.Seed9=loadSeedData(data.Seed9.elements);
+    $scope.Seed10=loadSeedData(data.Seed10.elements);
+    $scope.WC1=loadSeedData(_.union(data.Seed11.elements, data.Seed12.elements,data.Seed13.elements,data.Seed14.elements,data.Seed15.elements,data.Seed16.elements));
+    $scope.WC2=$scope.WC1;
+    function loadSeedData(SeedData){
+      var teams = [];// _.filter(SeedData, function(o) { return o.Team != "NO" && isNaN(o.Team); });
+      var players=[];
+      var currTeam = ""
+      for(x=0; x<SeedData.length; x++){
+        if(SeedData[x].Team!="NO" && isNaN(SeedData[x].Team) && SeedData[x].Team!="--"){
+          currTeam = SeedData[x].Team
+          teams.push({name: currTeam, value: currTeam});
+        } else {
+          if(SeedData[x].Team!="NO")
+            players.push({team: currTeam, name: SeedData[x].Player, value: SeedData[x].Player})
         }
-        return {teams, players};
       }
-    });
+      return {teams, players};
+    }
+  });
   }
 });
