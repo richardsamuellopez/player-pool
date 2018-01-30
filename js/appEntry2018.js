30c84e06a0ecdf63168ba42b6409d5d96eb62e4d
richardsamuellopez <richardsameullopez@gmail.com>
2018-01-29 22:06:21 -0500

    testing

--- js/appEntry2018.js
+++ js/appEntry2018.js
@@ -30,6 +30,15 @@ angular
     if($scope.email && $scope.entryName && $scope.seed1Player && $scope.seed2Player && $scope.seed3Player && $scope.seed4Player && $scope.seed5Player && $scope.seed6Player && $scope.seed7Player && $scope.seed8Player && $scope.seed9Player && $scope.seed10Player && $scope.WC1Player && $scope.WC2Player && $scope.seed1Team && $scope.seed2Team && $scope.seed3Team && $scope.seed4Team && $scope.seed5Team && $scope.seed6Team && $scope.seed7Team && $scope.seed8Team && $scope.seed9Team && $scope.seed10Team && $scope.WC1Team && $scope.WC2Team )
       $scope.isSubmitDisabled = true;
   };
+
+  $scope.submitEntryForm = function() {
+    $scope.entryData = {"entry.2120297232": $scope.email };
+         $http({
+             method : 'POST',
+             url : 'https://docs.google.com/forms/d/e/1FAIpQLSet4oUMSbXIM8plxyId3_LVBzAK4Yk_YDxrWd12MEsFadEHrQ/formResponse?',
+             data : $scope.entryData
+         })
+
   function LoadData(){Tabletop.then(function(ttdata){
     var currTS = new Date();
     // console.log("Load Data: ",currTS);
