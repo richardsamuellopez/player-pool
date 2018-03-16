261ab796c625abd2b12414669d3a8ee023ccbb08
richardsamuellopez <richardsameullopez@gmail.com>
2018-03-16 10:25:54 -0400

    Uncomment code

--- js/appStandings.js
+++ js/appStandings.js
@@ -58,59 +58,59 @@ $scope.reloadPage = function(){window.location.reload();}
       $scope.getSeed = function(num) {
           return new Array(num);
       }
-  //
-  //         // 32: "4"
-  //         // 64: "22"
-  //         // Championship: "0"
-  //         // Elite 8: "6"
-  //         // Final 4: "0"
-  //         // Player: "Stanley Johnson"
-  //         // Roster Count: "#REF!"
-  //         // Still Alive?: "No"
-  //         // Sweet 16: "12"
-  //         // Team: "Arizona"
-  //         // Total: "44"
-  //         $scope.getIndex = function(entry){
-  //          return $scope.entries.indexOf(entry)+1;
-  //        };
-  //         $scope.getPlayer = function(name){
-  //           var index =_.findIndex($scope.players,{'Player':name});
-  //           return _.find($scope.players,{'Player': name});
-  //         };
-  //
-  //         $scope.playerPoints = function(name){
-  //           var player = $scope.getPlayer(name)
-  //           // var ttlPts = parseInt(player["64"])+parseInt(player["32"])+parseInt(player["Sweet 16"])+
-  //           // parseInt(player["Elite 8"])+parseInt(player["Final 4"])+parseInt(player["Championship"]);
-  //           // return ttlPts;
-  //           return parseInt(player.Total);
-  //         };
-  //
-  //         $scope.teamPoints = function(team){
-  //           var points = 0;
-  //           for(i=1;i<10;i++){
-  //             points+=$scope.playerPoints(team[i]);
-  //           }
-  //           points+=$scope.playerPoints(team.WC1);
-  //           points+=$scope.playerPoints(team.WC2);
-  //           team.totalTeamPoints=points;
-  //           return points;
-  //         };
-  //
-  //         $scope.playerTeam = function(name){
-  //           var player = $scope.getPlayer(name);
-  //           return player.Team;
-  //         };
-  //
-  //         $scope.playerPointsByRound = function(name, round){
-  //           var player = $scope.getPlayer(name);
-  //           return player[round];
-  //         };
-  //
-  //         $scope.isEliminated = function(name){
-  //           var player=$scope.getPlayer(name);
-  //           return player.StillAlive==="No";
-  //         };
+
+          // 32: "4"
+          // 64: "22"
+          // Championship: "0"
+          // Elite 8: "6"
+          // Final 4: "0"
+          // Player: "Stanley Johnson"
+          // Roster Count: "#REF!"
+          // Still Alive?: "No"
+          // Sweet 16: "12"
+          // Team: "Arizona"
+          // Total: "44"
+          $scope.getIndex = function(entry){
+           return $scope.entries.indexOf(entry)+1;
+         };
+          $scope.getPlayer = function(name){
+            var index =_.findIndex($scope.players,{'Player':name});
+            return _.find($scope.players,{'Player': name});
+          };
+
+          $scope.playerPoints = function(name){
+            var player = $scope.getPlayer(name)
+            // var ttlPts = parseInt(player["64"])+parseInt(player["32"])+parseInt(player["Sweet 16"])+
+            // parseInt(player["Elite 8"])+parseInt(player["Final 4"])+parseInt(player["Championship"]);
+            // return ttlPts;
+            return parseInt(player.Total);
+          };
+
+          $scope.teamPoints = function(team){
+            var points = 0;
+            for(i=1;i<10;i++){
+              points+=$scope.playerPoints(team[i]);
+            }
+            points+=$scope.playerPoints(team.WC1);
+            points+=$scope.playerPoints(team.WC2);
+            team.totalTeamPoints=points;
+            return points;
+          };
+
+          $scope.playerTeam = function(name){
+            var player = $scope.getPlayer(name);
+            return player.Team;
+          };
+
+          $scope.playerPointsByRound = function(name, round){
+            var player = $scope.getPlayer(name);
+            return player[round];
+          };
+
+          $scope.isEliminated = function(name){
+            var player=$scope.getPlayer(name);
+            return player.StillAlive==="No";
+          };
 
       }
     });
